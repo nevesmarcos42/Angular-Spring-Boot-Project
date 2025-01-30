612e223d5345fbf999d60a5bc5f478d1e578f1d8
nevesmarcos42 <nevesmarcos42@gmail.com>
2025-01-30 09:18:11 -0300

    Primeiro commit - Estrura de pasta api backend

---
+++ api-records-crud/src/test/java/com/neves/api_records_crud/ApiRecordsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neves.api_records_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRecordsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
